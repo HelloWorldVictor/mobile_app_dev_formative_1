9f10032c20fa9869c2444f610f719cf6e83f892c
Elvin100s <e.cyubahiro@alustudent.com>
2026-02-09 13:13:28 +0200

    Added routes configuration

--- lib/utils/routes.dart
+++ lib/utils/routes.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
 import '../screens/main_screen.dart';
 import '../screens/dashboard_screen.dart';
+import '../screens/assignments_screen.dart';
+import '../screens/schedule_screen.dart';
+import '../screens/add_assignment_screen.dart';
+import '../screens/add_session_screen.dart';
+import '../models/assignment.dart';
+import '../models/session.dart';
 
 /// Route names
 class AppRoutes {
@@ -23,6 +29,30 @@ class RouteGenerator {
 
       case AppRoutes.dashboard:
         return MaterialPageRoute(builder: (_) => const DashboardScreen());
+
+      case AppRoutes.assignments:
+        return MaterialPageRoute(builder: (_) => const AssignmentsScreen());
+
+      case AppRoutes.schedule:
+        return MaterialPageRoute(builder: (_) => const ScheduleScreen());
+
+      case AppRoutes.addAssignment:
+        return MaterialPageRoute(builder: (_) => const AddAssignmentScreen());
+
+      case AppRoutes.editAssignment:
+        final assignment = settings.arguments as Assignment?;
+        return MaterialPageRoute(
+          builder: (_) => AddAssignmentScreen(assignment: assignment),
+        );
+
+      case AppRoutes.addSession:
+        return MaterialPageRoute(builder: (_) => const AddSessionScreen());
+
+      case AppRoutes.editSession:
+        final session = settings.arguments as Session?;
+        return MaterialPageRoute(
+          builder: (_) => AddSessionScreen(session: session),
+        );
 
       default:
         return _errorRoute(settings.name);
